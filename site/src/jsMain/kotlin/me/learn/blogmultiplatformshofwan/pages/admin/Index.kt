170a534587ab7d0f9b8583ef21eebd2033f4c61a
Shofwan <mshofwan.dev@gmail.com>
2023-10-18 15:56:50 +0700

    [Section 2: Admin Panel - Login Screen][15] Redirect Unauthorized users

--- site/src/jsMain/kotlin/me/learn/blogmultiplatformshofwan/pages/admin/Index.kt
+++ site/src/jsMain/kotlin/me/learn/blogmultiplatformshofwan/pages/admin/Index.kt
@@ -2,9 +2,18 @@ package me.learn.blogmultiplatformshofwan.pages.admin
 
 import androidx.compose.runtime.Composable
 import com.varabyte.kobweb.core.Page
+import me.learn.blogmultiplatformshofwan.utils.isUserLoggedIn
+import org.jetbrains.compose.web.dom.Text
 
 @Page
 @Composable
-fun HomePage() {
+fun HomeScreen() {
+    isUserLoggedIn {
+        HomePage()
+    }
+}
 
+@Composable
+fun HomePage() {
+    Text("Admin Home Page")
 }
--- site/src/jsMain/kotlin/me/learn/blogmultiplatformshofwan/utils/ApiFunctions.kt
+++ site/src/jsMain/kotlin/me/learn/blogmultiplatformshofwan/utils/ApiFunctions.kt
@@ -2,11 +2,11 @@ package me.learn.blogmultiplatformshofwan.utils
 
 import com.varabyte.kobweb.browser.api
 import kotlinx.browser.window
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import me.learn.blogmultiplatformshofwan.models.User
 import me.learn.blogmultiplatformshofwan.models.UserSafe
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.decodeFromString
 
 suspend fun checkUserExistence(user: User): UserSafe? {
     return try {
@@ -16,8 +16,21 @@ suspend fun checkUserExistence(user: User): UserSafe? {
         )
 
         result?.decodeToString()?.let { Json.decodeFromString<UserSafe>(it) }
-    } catch (e: Exception){
+    } catch (e: Exception) {
         println(e.message)
         null
+    }
+}
+
+suspend fun checkUserId(id: String?): Boolean {
+    return try {
+        val result = window.api.tryPost(
+            apiPath = "check-userid",
+            body = Json.encodeToString(id).encodeToByteArray()
+        )
+        result?.decodeToString()?.let { Json.decodeFromString<Boolean>(it) } ?: false
+    }catch (e:Exception){
+        println(e.message)
+        false
     }
 }

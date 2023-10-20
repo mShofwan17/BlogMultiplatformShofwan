1e0fdd8add3d9b1a67b36655422933e6a324b5a0
Shofwan <mshofwan.dev@gmail.com>
2023-10-20 14:33:13 +0700

    [Section 3: Admin Panel - Home Screen][24] Create AddButton

--- site/src/jsMain/kotlin/me/learn/blogmultiplatformshofwan/pages/admin/Index.kt
+++ site/src/jsMain/kotlin/me/learn/blogmultiplatformshofwan/pages/admin/Index.kt
@@ -1,23 +1,37 @@
 package me.learn.blogmultiplatformshofwan.pages.admin
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import com.varabyte.kobweb.compose.css.Cursor
 import com.varabyte.kobweb.compose.foundation.layout.Box
-import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.graphics.Colors
+import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
+import com.varabyte.kobweb.compose.ui.modifiers.borderRadius
+import com.varabyte.kobweb.compose.ui.modifiers.color
+import com.varabyte.kobweb.compose.ui.modifiers.cursor
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.height
+import com.varabyte.kobweb.compose.ui.modifiers.margin
 import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.onClick
+import com.varabyte.kobweb.compose.ui.modifiers.position
+import com.varabyte.kobweb.compose.ui.modifiers.size
+import com.varabyte.kobweb.compose.ui.styleModifier
 import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.core.rememberPageContext
+import com.varabyte.kobweb.silk.components.icons.fa.FaPlus
+import com.varabyte.kobweb.silk.components.icons.fa.IconSize
+import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
 import me.learn.blogmultiplatformshofwan.components.AdminPageLayout
-import me.learn.blogmultiplatformshofwan.components.OverflowSidePanel
-import me.learn.blogmultiplatformshofwan.components.SidelPanel
-import me.learn.blogmultiplatformshofwan.utils.Constant.PAGE_WIDTH
+import me.learn.blogmultiplatformshofwan.models.Theme
+import me.learn.blogmultiplatformshofwan.navigation.Screen
+import me.learn.blogmultiplatformshofwan.utils.Constant
 import me.learn.blogmultiplatformshofwan.utils.isUserLoggedIn
+import org.jetbrains.compose.web.css.Position
 import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.css.vh
 
 @Page
 @Composable
@@ -31,6 +45,49 @@ fun HomePage() {
 @Composable
 fun HomeScreen() {
     AdminPageLayout {
+        AddButton()
+    }
+}
 
+@Composable
+fun AddButton() {
+    val breakpoint = rememberBreakpoint()
+    val context = rememberPageContext()
+    Box(
+        modifier = Modifier
+            .height(100.vh)
+            .fillMaxWidth()
+            .maxWidth(Constant.PAGE_WIDTH.px)
+            .position(Position.Fixed)
+            .styleModifier {
+                property("pointer-events", "none")
+            },
+        contentAlignment = Alignment.BottomEnd
+    ) {
+        Box(
+            modifier = Modifier
+                .margin(
+                    right = if (breakpoint > Breakpoint.MD) 40.px else 20.px,
+                    bottom = if (breakpoint > Breakpoint.MD) 40.px else 20.px,
+                )
+                .size(
+                    if (breakpoint > Breakpoint.MD) 80.px else 40.px
+                )
+                .backgroundColor(Theme.PrimaryColor.rgb)
+                .borderRadius(r = 14.px)
+                .cursor(Cursor.Pointer)
+                .onClick {
+                    context.router.navigateTo(Screen.Admin.Create.route)
+                }
+                .styleModifier {
+                    property("pointer-events", "auto")
+                },
+            contentAlignment = Alignment.Center
+        ) {
+            FaPlus(
+                modifier = Modifier.color(Colors.White),
+                size = IconSize.LG
+            )
+        }
     }
 }

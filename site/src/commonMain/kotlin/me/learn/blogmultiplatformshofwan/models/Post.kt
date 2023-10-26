9822577c50418489602a7f4de5372b2650634bb9
mshof <mshofwan.dev@gmail.com>
2023-10-26 16:56:22 +0700

    [Section 4: Admin Panel - Create Screen][34] Crate a Post Model Class

---
+++ site/src/commonMain/kotlin/me/learn/blogmultiplatformshofwan/models/Post.kt
@@ -0,0 +1,15 @@
+package me.learn.blogmultiplatformshofwan.models
+
+expect class Post {
+    val id: String
+    val authorName: String
+    val date: Long
+    val title: String
+    val subtitle: String
+    val thumbnail: String
+    val detail: String
+    val category: Category
+    val popular: Boolean
+    val main: Boolean
+    val sponsored: Boolean
+}
